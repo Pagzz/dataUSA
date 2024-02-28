a932eded7a6e0c971fbfabc8d5c5cdecdb3ef92d
Pagzz <150529712+Pagzz@users.noreply.github.com>
2024-02-28 09:58:15 -0500

    Add files via upload

---
+++ dataType.py
@@ -0,0 +1,33 @@
+import aiohttp
+import asyncio
+
+async def get_auth_token():
+    async with aiohttp.ClientSession() as session:
+        url = 'http://www.datairis.co/V1/auth/subscriber/'
+        headers = {
+            'SubscriberUsername': 'mintmediaapp',
+            'SubscriberPassword': 'FNCcKWyi0bAXnoPAka8L',
+            'SubscriberID': '224',
+        }
+        params = {
+            'AccessToken': 'ad381-a59ec-4d7d',
+        }
+        async with session.get(url, headers=headers, params=params) as response:
+            data = await response.json()
+            return data['Response']['responseDetails']['TokenID']
+
+async def get_mapped_dbs(token_id):
+    async with aiohttp.ClientSession() as session:
+        url = 'http://www.datairis.co/V1/search/mapped/database'
+        headers = {'TokenID': token_id}
+        async with session.get(url, headers=headers) as response:
+            data = await response.json()
+            return data
+
+async def main():
+    token_id = await get_auth_token()
+    mapped_dbs = await get_mapped_dbs(token_id)
+    print(mapped_dbs)
+
+if __name__ == '__main__':
+    asyncio.run(main())
